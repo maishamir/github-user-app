ecb8db250f24c1524b86b46128307692252f0ee4
Maisha Mir <maisha.mir2014@gmail.com>
2025-11-15 17:59:43 -0800

    basic styling - not dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.scss";
+import Header from "./components/Header/Header";
 import SearchBar from "./components/SearchBar/SearchBar";
 
 function App() {
   return (
     <main className="app">
+      <Header />
       <SearchBar />
       devfinder Light Dark  Search Joined Repos
       Followers Following
